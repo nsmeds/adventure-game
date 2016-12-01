3825952349ab91e6b8e8b5659b4ebc745d1210ea
JPGothro <jpgothro@gmail.com>
2016-11-30 21:49:46 -0800

    help info

---
+++ src/help.js
@@ -0,0 +1,40 @@
+
+const helpOpts = {
+    allChoices: [
+        {
+            title: 'choiceNorth',
+            name: 'Move North',
+            command: 'go n'
+        },
+        {
+            title: 'choiceSouth',
+            name: 'Move South',
+            command: 'go s'
+        },
+        {
+            title: 'choiceEast',
+            name: 'Move East',
+            command: 'go e'
+        },
+        {
+            title: 'choiceWest',
+            name: 'Move West',
+            command: 'go w'
+        },
+        {
+            title: 'choiceTakeItem',
+            name: 'Take Item',
+            command: 'take item'
+        },
+        {
+            title: 'choiceDropItem',
+            name: 'Drop Item',
+            command: 'drop item'
+        },
+        {
+            title: 'choiceUseItem',
+            name: 'Use Item',
+            command: 'use item'
+        }
+    ]
+};
