6178f0fc01a7ab1781c9336abf90bc15609ed5c4
nsmeds <niilosmeds@gmail.com>
2016-12-01 14:36:28 -0800

    working on displaying front-end messaging

--- src/interface.js
+++ src/interface.js
@@ -1,15 +1,4 @@
-import angular from 'angular';
-// import './interface';
-
-const app = angular.module('myApp', []);
-
-app.controller('myGame', ['$scope', function($scope) {
-    $scope.userAction = 'wha';
-    $scope.newText = $scope.userAction;
-    $scope.playerHistory = [];
-}]);
-
-app.controller('interface', ['$scope', function($scope) {
+export default function interaction($scope) {
     $scope.progress = [
         {
             text: 'You are in an empty room. There are two adjacent rooms - one contains a weapon and one contains a monster. What do you do?'
@@ -28,7 +17,6 @@ app.controller('interface', ['$scope', function($scope) {
         if (entry.text.toLowerCase() === 'test') {
             $scope.progress.push('You have made progress.');
         }
-    }    
-}]);
+    }
+}
 
-export default app;
