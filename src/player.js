2fc94effd4819d3477ff8be2bf0b0d0a708c8970
JPGothro <jpgothro@gmail.com>
2016-11-30 16:41:38 -0800

    creating a player object

---
+++ src/player.js
@@ -0,0 +1,15 @@
+
+const thePlayer = {
+    inventory: [],
+    location: {},
+    action(cmd){
+        let theActions = cmd.split(' ');
+        if (theActions[0] = 'get') {
+            this.inventory.push(theActions[1]);
+        } else {
+            this.location = theActions[0].location;
+        };
+    }
+};
+
+export default thePlayer;
